0882fa723543df86d3204944e0700d4296b0e21d
Haiyan Hazrin Bin Haimidon <hazrinhaiyan987@gmail.com>
2024-12-08 13:46:46 +0700

    Add project files.

---
+++ Models/Riddle.cs
@@ -0,0 +1,14 @@
+﻿namespace RiddlesWebApp.Models
+{
+    public class Riddle
+    {
+        public int id { get; set; }
+        public String Question { get; set; }
+        public String Answer { get; set; }
+
+        public Riddle()
+        {
+            
+        }
+    }
+}
